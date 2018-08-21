b8be45c69a791c50c1c61e50194a94e8a517f86e
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-08-21 16:49:23 -0400

    level design/path creation

---
+++ Assets/Project/Scripts/NavTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavTile : MonoBehaviour {
+
+    public bool navigable;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
